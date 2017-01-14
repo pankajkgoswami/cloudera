c5630a1e1399c38a9dbd7dac7880e2126fe45225
Pankaj <pankajkumar.goswami@wipro.com>
2017-01-14 23:06:22 +0530

    UDF Function Updated with sample data and Steps

--- HiveUDF/src/UDF/UDFZodiacSign.java
+++ HiveUDF/src/UDF/UDFZodiacSign.java
@@ -1,6 +1,8 @@
 package UDF;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import org.apache.hadoop.hive.ql.exec.Description;
@@ -16,19 +18,27 @@ private SimpleDateFormat df;
 public UDFZodiacSign(){
 df = new SimpleDateFormat("MM-dd-yyyy");
 }
-@SuppressWarnings("deprecation")
+
 public String evaluate( Date bday ){
-return this.evaluate( bday.getMonth(), bday.getDay() );
+	Date date = null;
+	Calendar cal = Calendar.getInstance();
+	cal.setTime(date);
+	return this.evaluate( cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
 }
-public String evaluate(String bday){
+
+public String evaluate(String bday) throws ParseException{
 Date date = null;
-try {
 date = df.parse(bday);
+Calendar cal = Calendar.getInstance();
+cal.setTime(date);
+try {
+cal.get(Calendar.MONTH);
 } catch (Exception ex) {
 return null;
 }
-return this.evaluate( date.getMonth()+1, date.getDay() );
+return this.evaluate( cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH));
 }
+
 public String evaluate( Integer month, Integer day ){
 if (month==1) {
 if (day < 20 ){
@@ -44,7 +54,78 @@ return "Aquarius";
 return "Pisces";
 }
 }
-/* ...other months here */
+if (month==3){
+if (day < 21 ){
+return "Pisces";
+} else {
+return "Aries";
+}
+}
+if (month==4){
+if (day < 20 ){
+return "Aries";
+} else {
+return "Taurus";
+}
+}
+
+if (month==5){
+if (day < 20 ){
+return "Taurus";
+} else {
+return "Gemini";
+}
+}
+
+if (month==6){
+if (day < 21 ){
+return "Gemini";
+} else {
+return "Cancer";
+}
+}
+if (month==7){
+if (day < 23 ){
+return "Cancer";
+} else {
+return "Leo";
+}
+}
+if (month==8){
+if (day < 23 ){
+return "Leo";
+} else {
+return "Virgo";
+}
+}
+if (month==9){
+if (day < 23 ){
+return "Virgo";
+} else {
+return "Libra";
+}
+}
+if (month==10){
+if (day < 23 ){
+return "Libra";
+} else {
+return "Scorpio";
+}
+}
+if (month==11){
+if (day < 22 ){
+return "Scorpio";
+} else {
+return "Saggitarius";
+}
+}
+if (month==2){
+if (day < 19 ){
+return "Saggitarius";
+} else {
+return "Capricorn";
+}
+}
 return null;
 }
 }
